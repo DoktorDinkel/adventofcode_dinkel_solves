6519605da341eb31cce04441ff97cda1076d9d7d
DoktorDinkel <40692379+DoktorDinkel@users.noreply.github.com>
2020-12-03 15:17:50 +0100

    day3 update

--- day3/day3A.py
+++ day3/day3A.py
@@ -5,23 +5,34 @@ imp2 = open(imp,"+r").read()
 
 imp2 = imp2.split("\n")
 
-print("-------------------------")
-print("ARRAY LENGTH:",len(imp2))
-print("First slot:",imp2[0])
-print("-------------------------")
+#print("-------------------------")
+#print("ARRAY LENGTH:",len(imp2))
+#print("First slot:",imp2[0])
+#print("-------------------------")
 
 currentStep = 0
-currentString = ""
-totalString = ""
+totalString = imp2[0]
+trees = 0
 
 for x in range(len(imp2)):
 
     currentString = imp2[x]
+    totalString=currentString
 
-    print("  currentString:",currentString,"  currentStep:", currentStep,"  lengthTotalString:", len(totalString))
+    #print("currentString:",currentString,"  currentStringLength:",len(currentString),"  currentStep:", currentStep,"  lengthTotalString:", len(totalString))
 
-    if currentStep > len(totalString):
+    while currentStep >= len(totalString):
         totalString = totalString + currentString
-        print("long")
+        #print("Added:",len(currentString))
+
+    #print("lenthTotalString:",len(totalString),"currentStep:",currentStep)
+    #print("------------------------------------------------------------------------------------------------------------------------------")
+
+    if totalString[currentStep] == "#":
+        trees = trees + 1
 
     currentStep = currentStep + 3
+
+    print(totalString)
+
+print("Total trees detected:",trees)
