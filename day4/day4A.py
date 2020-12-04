6824c8753d19f63dc5236aaa6eaadd89ccf32e11
DoktorDinkel <40692379+DoktorDinkel@users.noreply.github.com>
2020-12-04 11:57:55 +0100

    Update day4A.py

--- day4/day4A.py
+++ day4/day4A.py
@@ -5,10 +5,20 @@ imp2 = open(imp,"+r").read()
 
 imp2 = imp2.split("\n\n")
 
+matches = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+total = 0
+
 print("-------------------------------------------------------------------------")
 print("Lenght:",len(imp2),"strings")
 
 for x in range(len(imp2)):
-    print(imp2[x])
+    currentString = imp2[x]
+    print(currentString)
 
-print("")
+    if all(x in currentString for x in matches):
+        print("success!")
+        total = total + 1
+
+    print("-------------------------------------------------------------------------")
+
+print("Total valid:",total)
