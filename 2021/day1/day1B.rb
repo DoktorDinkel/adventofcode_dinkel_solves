fdf98f3abf76ca1b22da0e83f6aca48f144f9ec3
DoktorDinkel <40692379+DoktorDinkel@users.noreply.github.com>
2021-12-01 11:55:58 +0100

    Completed Day 1 A&B

--- 2021/day1/day1B.rb
+++ 2021/day1/day1B.rb
@@ -1,16 +1,29 @@
 measurements = File.readlines("input_day1.txt")
 
-def createSum(arrayIndex)
+def createSum(arrayIndex, measurements)
     i = 0
     sum = 0
     while i <= 2
-        sum = sum + measurements[arrayIndex+i]
+        sum = sum + measurements[arrayIndex+i].to_i
         i += 1
     end
 
     return sum
 end
 
+arrayIndex = 1
+answer = 0
+previousSum = createSum(0, measurements)
 
+while arrayIndex - 2 < measurements.length
+    currentSum = createSum(arrayIndex, measurements)
+    if previousSum < currentSum
+        answer += 1
+    end
 
+    previousSum = currentSum
+    arrayIndex += 1
+end
+
+puts answer
 #create sums of three, compare the sums and count as in the previous puzzle, stop when there aren't enough numbers to create a sum
