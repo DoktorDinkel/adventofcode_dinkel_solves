70f5d06aad8d46db2a7e22dfd0992351e3c8e66c
DoktorDinkel <40692379+DoktorDinkel@users.noreply.github.com>
2022-12-06 11:20:51 +0100

    Day 6 WIP

---
+++ 2022/day6/day6A.rb
@@ -0,0 +1,20 @@
+data = File.readlines("input_day6.txt")
+
+puts data
+
+char1 = ""
+char2 = ""
+char3 = ""
+char4 = ""
+
+while i < data.length
+    currentChar = data[i]
+
+    char1 = data[i]
+    char2 = data[i+1]
+    char3 = data[i+2]
+    char4 = data[i+3]
+
+    charArray
+
+    while j < 4
